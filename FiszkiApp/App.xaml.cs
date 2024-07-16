520abb238fe8c2909c79d0c44d22bdb234b8b2c2
Miłosz <milystach@gmail.com>
2024-07-16 17:59:44 +0200

    dodanie CS i nugetów

--- FiszkiApp/App.xaml.cs
+++ FiszkiApp/App.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace FiszkiApp
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace FiszkiApp
 {
     public partial class App : Application
     {
+        public static IConfiguration Configuration { get; private set; }
         public App()
         {
             InitializeComponent();
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData))
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
 
             MainPage = new AppShell();
         }
---
+++ FiszkiApp/dbConnetcion/DatabaseService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace FiszkiApp.dbConnetcion
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService()
+        {
+            _connectionString = App.Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<DataTable> GetDataAsync(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+    }
+}
