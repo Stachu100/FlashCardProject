554ad550a9b0f5c9cfbaefcb6547d411687eb863
Miłosz <milystach@gmail.com>
2024-09-08 19:22:39 +0200

    Testowe dodanie flagi

---
+++ FiszkiApp/EntityClasses/Item.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FiszkiApp.EntityClasses
+{
+    public class Item : ObservableObject
+    {
+        public string Name { get; set; }
+        public ImageSource Image { get; set; }
+    }
+}
--- FiszkiApp/ViewModel/ProfileViewModel.cs
+++ FiszkiApp/ViewModel/ProfileViewModel.cs
@@ -9,14 +9,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+using FiszkiApp.EntityClasses;
 
 namespace FiszkiApp.ViewModel
 {
     public partial class ProfileViewModel : MainViewModel
     {
+
+        public IAsyncRelayCommand LoadCountriesCommand { get; }
+        public ObservableCollection<ImageSource> Items { get; } = new ObservableCollection<ImageSource>();
+        //public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
+
+        [ObservableProperty]
+        private byte[] imageAsBytes;
+
+        [ObservableProperty]
+        private string user;
+
+        [ObservableProperty]
+        private string country;
+
+        [ObservableProperty]
+        private ObservableCollection<string> countryPicker;
+
+        [ObservableProperty]
+        private string countryPicked;
+        partial void OnCountryPickedChanged(string value)
+        {
+                AddItem(value, "Flags/flag_poland.jpg");
+                CountryPicked = null;            
+        }
+
         public ProfileViewModel(AuthService authService)
         {
-            _authService = authService;            
+            _authService = authService;
+            LoadCountriesCommand = new AsyncRelayCommand(LoadCountry);
+            LoadCountriesCommand.Execute(null);
         }
 
         public async Task OnNavigatedTo(NavigationEventArgs args)
@@ -31,20 +61,16 @@ namespace FiszkiApp.ViewModel
                 ImageAsBytes = uploadedImage;
             }
             User = user;
-            Country = country;
+            Country = "Kraj pochodzenia: " + country;
 
         }
 
-        [ObservableProperty]
-        private byte[] imageAsBytes;
-
-        [ObservableProperty]
-        private string user;
-
-        [ObservableProperty]
-        private string country;
-
-        
+        private async Task LoadCountry()
+        {
+            var countriesDic = new dbConnetcion.SQLQueries.CountriesDic();
+            var countries = await countriesDic.Countries();
+            CountryPicker = new ObservableCollection<string>(countries);
+        }
 
         private readonly AuthService _authService;
 
@@ -54,5 +80,37 @@ namespace FiszkiApp.ViewModel
             _authService.Logout();
             Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
         }
+
+        void AddItem(string strValue, string imagePath)
+        {
+            if (!string.IsNullOrEmpty(strValue))
+            {
+
+                ImageSource imgSource = ImageSource.FromFile(imagePath);
+
+
+                if (!Items.Contains((imgSource)))
+                // if (!Items.Any(item => item.Text == strValue && item.Image == imgSource))
+                {
+                    Items.Add((imgSource));
+                    //Items.Add(new Item { Text = strValue, Image = imgSource });
+                    CountryPicked = null;
+                }
+            }
+        }
+        //private void AddItem(string name, string imageName)
+        //{
+        //    if (!string.IsNullOrEmpty(name))
+        //    {
+        //        ImageSource imgSource = ImageSource.FromFile(imageName);
+        //        var item = new EntityClasses.Item { Name = name, Image = imgSource };
+
+        //        // Sprawdź, czy element już istnieje, zanim go dodasz
+        //        if (!Items.Any(i => i.Name == name && i.Image == imgSource))
+        //        {
+        //            Items.Add(item);
+        //        }
+        //    }
+        //}
     }
 }
